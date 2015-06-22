6eb5eaeacd9f5da2b76525b8841cd6b96d722bb2
Ashok Menon <amenon94@gmail.com>
2015-06-22 22:17:00 +0100

    Talk: C++ Lifetime examples.

---
+++ cpp/leaky.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int *mk_int() { return new int; }
+
+int *sum(int lo, int hi)
+{
+  int *acc = mk_int();
+  for(int i = lo; i <= hi; ++i)
+    *acc += i;
+
+  return acc;
+}
+
+int main(int, char **)
+{
+  for(int i = 0; i < 1000; ++i) {
+    int *x = sum(0, i);
+    cout << "Sum from 0 to " << i << ": " << *x << "\n";
+  }
+
+  return 0;
+}
